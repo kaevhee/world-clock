ba9db3b2838912b452afd5f616c605c207e77d59
kaweng pam <kawengpam56@gmail.com>
2024-10-23 15:24:48 +0100

    updates the clock functionality

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,19 @@
-// alert("welcome!")
+
+function updateTime(){//los angeles
+let losAngelesElement = document.querySelector("#los-angeles");
+let losAngelesDateElement = losAngelesElement.querySelector(".date");
+let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+losAngelesDateElement.innerHTML = moment().format("dddd Do MMMM yyyy");
+losAngelesTimeElement.innerHTML = moment().tz("America/Los_Angeles").format("h:mm:ss [<small>]A[</small>]")
+
+//paris
+let parisElement = document.querySelector("#paris");
+let parisDateElement = parisElement.querySelector(".date");
+let parisTimeElement = parisElement.querySelector(".time");
+parisDateElement.innerHTML = moment().format("dddd Do MMMM yyyy");
+parisTimeElement.innerHTML = moment()
+  .tz("Europe/Paris")
+  .format("h:mm:ss: [<small>]A[</small>]");
+}
+updateTime()
+setInterval(updateTime, 1000)
