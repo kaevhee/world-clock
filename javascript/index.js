46606b0889a33c758faa31fe9bb535ce66d9c354
kaweng pam <kawengpam56@gmail.com>
2024-10-23 20:15:50 +0100

    included a currrent time zone locator

--- javascript/index.js
+++ javascript/index.js
@@ -2,31 +2,35 @@ function updateTime() {
   //los angeles
   let losAngelesElement = document.querySelector("#los-angeles");
   if (losAngelesElement) {
-  let losAngelesDateElement = losAngelesElement.querySelector(".date");
-  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
-  losAngelesDateElement.innerHTML = moment().format("dddd Do MMMM yyyy");
-  losAngelesTimeElement.innerHTML = moment()
-    .tz("America/Los_Angeles")
-    .format("h:mm:ss [<small>]A[</small>]");
+    let losAngelesDateElement = losAngelesElement.querySelector(".date");
+    let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+    losAngelesDateElement.innerHTML = moment().format("dddd Do MMMM yyyy");
+    losAngelesTimeElement.innerHTML = moment()
+      .tz("America/Los_Angeles")
+      .format("h:mm:ss [<small>]A[</small>]");
   }
   //paris
   let parisElement = document.querySelector("#paris");
-  if (parisElement){
-  let parisDateElement = parisElement.querySelector(".date");
-  let parisTimeElement = parisElement.querySelector(".time");
-  parisDateElement.innerHTML = moment().format("dddd Do MMMM yyyy");
-  parisTimeElement.innerHTML = moment()
-    .tz("Europe/Paris")
-    .format("h:mm:ss: [<small>]A[</small>]");
-}
+  if (parisElement) {
+    let parisDateElement = parisElement.querySelector(".date");
+    let parisTimeElement = parisElement.querySelector(".time");
+    parisDateElement.innerHTML = moment().format("dddd Do MMMM yyyy");
+    parisTimeElement.innerHTML = moment()
+      .tz("Europe/Paris")
+      .format("h:mm:ss: [<small>]A[</small>]");
+  }
 }
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
+
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
-  console.log(cityTimeZone)
+  console.log(cityTimeZone);
   citiesElement.innerHTML = `
   <div class="city">
       <div>
